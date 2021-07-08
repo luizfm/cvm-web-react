bbe6d9a366a58ac4b747f99bf8ebb9bb5c1a3ac5
Proctux <luiz.ado@hotmail.com>
2021-07-07 23:28:00 -0300

    feat: Add some css media-queries. Need to finish the whole responsive CSS

--- src/views/presentation/index.jsx
+++ src/views/presentation/index.jsx
@@ -5,6 +5,7 @@ import kids from '../../assets/kids.svg';
 import Button, { BUTTON_SIZES, BUTTON_THEMES } from '../../components/button'
 
 import styles from './styles.module.scss'
+import classNames from 'classnames';
 
 const Presentation = () => {
     return (
@@ -13,8 +14,6 @@ const Presentation = () => {
                 <img className={styles.logo} src={logoImg} alt="CVM Logo"/>
 
                 <h1 className={styles.introduction}>A CVM ajuda a comunidade com seus serviços de creche e orfanato</h1>
-
-                <img className={styles.kids} src={kids} alt="Kids" />
 
                 <Button
                     className={styles['nav-button']}
@@ -26,6 +25,22 @@ const Presentation = () => {
                     Ir para o site
                 </Button>
             </section>
+            <div className={styles.aside}>
+                <img className={styles.kids} src={kids} alt="Kids" />
+
+                <Button
+                    className={styles['mobile-button']}
+                    id="navigate-home-button"
+                    to="/home"
+                    size={BUTTON_SIZES.LARGE}
+                    theme={BUTTON_THEMES.PRIMARY}
+                >
+                    Acessar o site
+                </Button>
+            </div>
+
+            <div className={classNames(styles.top, styles.rotated)} />
+            <div className={classNames(styles.bottom, styles.rotated)} />
         </div>
     )
 }
