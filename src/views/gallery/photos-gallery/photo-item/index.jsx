e670acd93f14140cb5f63b5e8e3ff4858be87702
Proctux <luiz.ado@hotmail.com>
2021-08-22 13:26:44 -0300

    feat: Add delete and add photos logic

--- src/views/gallery/photos-gallery/photo-item/index.jsx
+++ src/views/gallery/photos-gallery/photo-item/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 import classnames from 'classnames'
 import PropTypes from 'prop-types'
 
@@ -6,24 +6,31 @@ import Button from '../../../../components/button'
 
 import styles from './styles.module.scss'
 
-const PhotoItem = ({ label, image, imageDescription, className}) => {
+const PhotoItem = ({ label, image, imageDescription, id, onRemove, className }) => {
+  const handleOnRemove = useCallback(() => {
+    onRemove(id)
+  }, [id, onRemove])
+
   return (
     <section className={classnames(styles['elements-container'], className)}>
       <h1>{label}</h1>
       <img src={image} alt={imageDescription} />
-      <Button className={styles['delete-button']}>Remover</Button>
+      <Button className={styles['delete-button']} onClick={handleOnRemove}>Remover</Button>
     </section>
   )
 }
 
 PhotoItem.propTypes = {
+  id: PropTypes.number.isRequired,
   label: PropTypes.string,
   image: PropTypes.oneOfType([PropTypes.string, PropTypes.node]).isRequired,
   imageDescription: PropTypes.string.isRequired,
+  onRemove: PropTypes.func,
   className: PropTypes.string,
 }
 
 PhotoItem.defaultProps = {
+  onRemove: () => {},
   className: '',
   label: '',
 }
